889d981dd4a72b387e9241182e911dd3129346f9
Mohd Kashaf Siddiqui <siddiquikashaf786@gmail.com>
2020-08-20 20:14:04 +0530

    fixed error when entity not in lookup

--- actions.py
+++ actions.py
@@ -14,18 +14,22 @@ class ActionDisplayResults(Action):
     def name(self) -> Text:
         return "action_displayResults"    
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker,  domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        track=tracker.latest_message["entities"]
-        mongoConn=MongoClient("mongodb+srv://Admin:Admin@cluster0-q4yrs.mongodb.net/FP?retryWrites=true&w=majority")
-        myDb=mongoConn['ChatBot']
-        print(track)
-        search_value=str(track[0]['value'])
-        description="Not Found"
-        if myDb!=None:
-            myCol=myDb['keywords']
-            for i in list(myCol.find()):
-                if(i['keyword'].lower().find(search_value.lower())>=0):
-                    description=i['description']
-                    print(i)
-        message="I am displaying result for the keyword "+ search_value+'\n'+ description
-        dispatcher.utter_message(text=message)
+        try:
+            track=tracker.latest_message["entities"]
+            mongoConn=MongoClient("mongodb+srv://Admin:Admin@cluster0-q4yrs.mongodb.net/FP?retryWrites=true&w=majority")
+            myDb=mongoConn['ChatBot']
+            print(track)
+            search_value=str(track[0]['value'])
+            description="Not Found"
+            if myDb!=None:
+                myCol=myDb['keywords']
+                for i in list(myCol.find()):
+                    if(i['keyword'].lower().find(search_value.lower())>=0):
+                        description=i['description']
+                        print(i)
+            message="I am displaying result for the keyword "+ search_value+'\n'+ description
+            dispatcher.utter_message(text=message)            
+        except:
+            message="Keyword Not Found Please Try Again"
+            dispatcher.utter_message(text=message)
         return []
